ae7f1b8f544df37f27e1675c077f3a3cadf325ab
Ramin <ramin@motionworks.com.my>
2012-03-19 09:50:52 +0000

    Github login added

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ var express = require('express')
   , util = require('util');
 
 var passport = require('passport')
-  , FacebookStrategy = require('passport-facebook').Strategy;
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+  , GitHubStrategy = require('passport-github').Strategy;
 
+var GOOGLE_CLIENT_ID = "284339092631.apps.googleusercontent.com";
+var GOOGLE_CLIENT_SECRET = "xgPGPQTMv1Tuq-iUNCXzFO0M";
+var FACEBOOK_CLIENT_ID = "203298939775074";
+var FACEBOOK_CLIENT_SECRET = "b9a7bb0b88d306032a71d019a14139c0";
+var GITHUB_CLIENT_ID = "e34e53757d957fe5f3f1";
+var GITHUB_CLIENT_SECRET = "5b1977a1062d3d8d608eb4806602a09d3feeafe8";
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
 //   serialize users into and deserialize users out of the session.  Typically,
@@ -27,12 +35,44 @@ passport.deserializeUser(function(obj, done) {
 
 passport.use(new FacebookStrategy({
     //https://developers.facebook.com/apps/203298939775074/summary?web_hosting=0
-    clientID: "203298939775074",
-    clientSecret: "b9a7bb0b88d306032a71d019a14139c0",
+    clientID: FACEBOOK_CLIENT_ID,
+    clientSecret: FACEBOOK_CLIENT_SECRET,
     callbackURL: "http://node.raminv80.c9.io/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     /*User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return done(err, user);
+    });*/
+    //console.log(profile);
+    err = null;
+    user = profile;
+    return done(err, user);
+  }
+));
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://node.raminv80.c9.io/oauth2callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    /*User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });*/
+    //console.log(profile);
+    err = null;
+    user = profile;
+    return done(err, user);
+  }
+));
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://node.raminv80.c9.io/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    /*User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return done(err, user);
     });*/
     //console.log(profile);
@@ -84,6 +124,21 @@ app.get('/auth/facebook', passport.authenticate('facebook'),
     // The request will be redirected to Facebook for authentication, so this
     // function will not be called.
   });
+  
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile',
+                                            'https://www.googleapis.com/auth/userinfo.email'] }),
+  function(req, res){
+    // The request will be redirected to Google for authentication, so
+    // this function will not be called.
+  });
+  
+app.get('/auth/github',
+  passport.authenticate('github'),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
@@ -99,11 +154,26 @@ app.get('/auth/facebook/callback',
     res.redirect('/account');
   });
 
+app.get('/oauth2callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/account');
+  });
+  
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/account');
+  });
+
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
-
+                                      
+app.listen(app_port);
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
 //   the request is authenticated (typically via a persistent login session),
@@ -113,6 +183,4 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
 }
-                                      
-app.listen(app_port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
